f5a39dcd1551d943d115cb7999dc34b1139dc778
Gyeongjae Choi <def6488@gmail.com>
2020-08-13 16:46:11 +0900

    Create tweet.py

---
+++ tweet.py
@@ -0,0 +1,13 @@
+import config
+import twitter
+
+api = twitter.Api(
+    consumer_key=config.TWITTER_CONSUMER_KEY,
+    consumer_secret=config.TWITTER_CONSUMER_SECRET,
+    access_token_key=config.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret=config.TWITTER_ACCESS_TOKEN_SECRET,
+)
+
+
+def post(message):
+    api.PostUpdate(status=message)
